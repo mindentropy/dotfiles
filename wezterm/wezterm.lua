b5b4af2ec11872c35bc7c54aeb85a4efd3a70a04
mindentropy <mindentropy@gmail.com>
2025-09-14 01:48:54 +0530

    VIM, GVIM and Wezterm config customizations
    * Main cleanup of the customizations
    * Theme changes in the customizations

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- Changing color scheme
-config.color_scheme = "Gruvbox light, hard (base16)"
+-- config.color_scheme = "Gruvbox light, hard (base16)"
+-- config.color_scheme = "Nord Light (Gogh)"
+-- config.color_scheme = "nord-light"
 -- config.color_scheme = "Ef-Frost"
 -- config.color_scheme = "Gruvbox dark, hard (base16)"
 -- config.color_scheme = "Gruvbox dark, pale (base16)"
@@ -15,31 +17,41 @@ config.color_scheme = "Gruvbox light, hard (base16)"
 -- config.color_scheme = "Gruvbox dark, medium (base16)"
 -- config.color_scheme = "Gruvbox Material (Gogh)"
 -- config.color_scheme = "Tomorrow Night Blue"
+config.color_scheme = "tempus_totus"
 
 config.colors = {
-	foreground = "#292625", -- For light bg
+--	foreground = "#292625", -- For light bg
+--	background = "#fffadc", -- Customized light bg
 --	foreground = "#ebdbb2", -- For dark bg
-	cursor_border = "#e40000",
-	cursor_bg = "#e40000",
+--	cursor_border = "#e40000",
+--	cursor_bg = "#e40000",
 }
 
-config.font = wezterm.font("Iosevka Term SS16",
-					{stretch = "Normal", weight = "Medium",}
-				)
---config.font = wezterm.font("Iosevka Term SS16",
---					{stretch = "Normal", weight = "Normal",}
---				)
-config.font_size = 23.5
+config.font = wezterm.font("Inconsolata Medium",
+		    {
+--[[		        stretch = "Condensed",
+		        weight = "", ]]
+		    }
+		)
+
+config.inactive_pane_hsb = {
+    saturation = 1.0,
+    brightness = 1.0,
+}
+
+config.font_size = 27.5
+-- config.dpi = 109
+config.enable_wayland = false
+
 config.font_rules = {
 -- Bold but not italic
   {
 	intensity = 'Bold',
 	italic = false,
-
 	font = wezterm.font {
-		family = "Iosevka Term SS16",
-		weight = "Medium",
-		stretch = "Normal",
+		family = "Inconsolata Medium",
+--[[		stretch = "Condensed",
+		weight = "", ]]
     }
   },
 
@@ -48,21 +60,27 @@ config.font_rules = {
 	intensity = 'Bold',
 	italic = true,
 	font = wezterm.font {
-		family = "Iosevka Term SS16",
-		weight = "Medium",
+		family = "Inconsolata Medium",
 		italic = true,
-		stretch = "Normal",
+--[[		stretch = "Condensed",
+		weight = "",]]
     }
   },
 }
 
--- config.freetype_load_flags = "NO_HINTING"
+--[[
+For font config see the discussion comment below:
+https://github.com/wezterm/wezterm/discussions/3388#discussioncomment-5446306
+]]
 config.freetype_load_flags = "DEFAULT"
--- config.freetype_load_target = "Light"
-config.freetype_load_target = "Normal"
--- config.freetype_render_target = "Normal"
-config.freetype_render_target = "Normal"
-config.front_end = "WebGpu"
+config.freetype_load_flags = "NO_HINTING|FORCE_AUTOHINT"
+-- config.freetype_load_flags = "NO_HINTING|NO_AUTOHINT"
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
+--[[ config.freetype_load_target = "Normal"
+config.freetype_render_target = "Normal" ]]
+-- config.front_end = "WebGpu"
+config.front_end = "OpenGL"
 
 config.scrollback_lines = 100000
 config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1', 'CLIK=1' }
@@ -71,18 +89,23 @@ config.webgpu_power_preference = "HighPerformance"
 config.enable_csi_u_key_encoding = true
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
-config.default_cursor_style = "BlinkingBlock"
+config.default_cursor_style = "SteadyBlock"
 config.animation_fps = 10
-config.cursor_blink_rate = 250
-config.enable_csi_u_key_encoding = true
+config.cursor_blink_rate = 180
+config.enable_csi_u_key_encoding = false
 config.cursor_thickness = "200%"
+config.use_fancy_tab_bar = true
+config.audible_bell = "SystemBeep"
+config.window_background_opacity = 1.0
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
---config.force_reverse_video_cursor = true
+
+config.force_reverse_video_cursor = true
+config.term = "wezterm"
 
 return config
